3988fc6708afbe52be5a1d15b1a6461b42cc3e76
wenwenwen <118642740+wenjiaweiii@users.noreply.github.com>
2023-03-30 18:18:36 +0800

    Add files via upload
    GCD LCM

---
+++ a1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+signed main()
+{
+	int n,m;
+	cin >> n >> m;
+	int GCD=__gcd(n,m);
+	int LCM=n/__gcd(n,m)*m;
+	cout << "最大公因数为 ：" << GCD << endl;
+    cout << "最小公倍数为 ：" << LCM << endl;
+	return 0;
+}
