c01665e9223f3050ad6d359b01a2dc35cb724adf
wenwenwen <118642740+wenjiaweiii@users.noreply.github.com>
2023-03-30 20:18:56 +0800

    Add files via upload

--- a2.cpp
+++ a2.cpp
@@ -18,11 +18,12 @@ signed main()
 		cout << endl;
 	}while(next_permutation(a,a+n));
 	cout << "--------" << endl;
+	sort(a,a+n,greater<int>());
 	//逆序全排列
 	do{
 		for(int i:a)
 		cout << ' ' << i;
 		cout << endl;
-	}while(next_permutation(a,a+n));
+	}while(prev_permutation(a,a+n));
 	return 0;
 }
